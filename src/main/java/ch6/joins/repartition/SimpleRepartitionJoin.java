5b13ec60052f29a14f54975880efb9cdcdf0d5aa
Hua Yan <papayado@gmail.com>
2016-08-05 07:40:00 +0930

    add reduce join

---
+++ src/main/java/ch6/joins/repartition/SimpleRepartitionJoin.java
@@ -0,0 +1,16 @@
+package ch6.joins.repartition;
+
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.util.Tool;
+
+/**
+ * Created by hua on 04/08/16.
+ */
+public class SimpleRepartitionJoin extends Configured implements Tool {
+
+
+    @Override
+    public int run(String[] args) throws Exception {
+        return 0;
+    }
+}
