15a1f2942565c66fe70bb8352bf6cf338171a9fa
Hua Yan <papayado@gmail.com>
2016-08-11 18:54:20 +0930

    Add .

--- src/main/java/ch6/joins/bloom/BloomFilterCreator.java
+++ src/main/java/ch6/joins/bloom/BloomFilterCreator.java
@@ -1,6 +1,8 @@
 package ch6.joins.bloom;
 
 import ch6.joins.User;
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.mapred.AvroKey;
 import org.apache.hadoop.conf.Configured;
@@ -8,6 +10,7 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -17,7 +20,10 @@ import org.apache.hadoop.util.bloom.BloomFilter;
 import org.apache.hadoop.util.bloom.Key;
 import org.apache.hadoop.util.hash.Hash;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 
 /**
  * Created by hua on 08/08/16.
@@ -62,6 +68,42 @@ public class BloomFilterCreator extends Configured implements Tool{
     }
 
     public static class Reduce extends Reducer<NullWritable, BloomFilter, AvroKey<GenericRecord>, NullWritable> {
+        private BloomFilter filter = new BloomFilter(1000, 5, Hash.MURMUR_HASH);
 
+        @Override
+        protected void reduce(NullWritable key, Iterable<BloomFilter> values, Context context) throws IOException, InterruptedException {
+            for (BloomFilter bf: values) {
+                filter.or(bf);
+            }
+        }
+
+        @Override
+        protected void cleanup(Context context) throws IOException, InterruptedException {
+            context.write(new AvroKey<GenericRecord>(toGenericRecord(filter)), NullWritable.get());
+        }
+
+        public static final String BYTES_FIELD = "b";
+
+        private static final String SCHEMA_JSON =
+                "{\"type\": \"record\", \"name\": \"Bytes\", "
+                        + "\"fields\": ["
+                        + "{\"name\":\"" + BYTES_FIELD
+                        + "\", \"type\":\"bytes\"}]}";
+        public static final Schema SCHEMA = Schema.parse(SCHEMA_JSON);
+
+        public static GenericRecord toGenericRecord(Writable writable)
+                throws IOException {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            DataOutputStream dao = new DataOutputStream(baos);
+            writable.write(dao);
+            dao.close();
+            return toGenericRecord(baos.toByteArray());
+        }
+
+        public static GenericRecord toGenericRecord(byte[] bytes) {
+            GenericRecord record = new GenericData.Record(SCHEMA);
+            record.put(BYTES_FIELD, ByteBuffer.wrap(bytes));
+            return record;
+        }
     }
 }
